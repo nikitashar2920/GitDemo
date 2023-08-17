bd6e117e8f1c1bf3cb7952468387bd8cfee4806b
Nikita <sharnikki54@gmail.com>
2023-08-17 16:33:11 +0530

    The develop branch code

---
+++ DemoClass1.java
@@ -0,0 +1,15 @@
+
+public class DemoClass1 {
+
+	
+		
+		
+		@Test
+		public void testcase1()
+		{
+			System.out.println("This is my first test project");
+		}
+
+	
+
+}
---
+++ src/sharma/HelloWorld.java
@@ -0,0 +1,19 @@
+package sharma;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str = "Hello World";
+		String str2 = "Hello World2";
+		String str3 = "Hello World3";
+		System.out.println(str);
+		System.out.println(str2);
+		System.out.println(str3);
+		
+		
+
+	}
+
+}
