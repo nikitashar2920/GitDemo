73c53c4d74486a583286f2f5638da148ed2b52ef
Nikita <sharnikki54@gmail.com>
2023-08-17 15:09:38 +0530

    My second commit

---
+++ src/sharma/sharma2.java
@@ -0,0 +1,33 @@
+package sharma;
+
+public class sharma2 {
+
+	public static void main(String[] args) {
+	
+		// TODO Auto-generated method stub
+		
+int[] arr = {1,2,4,5,6,8,12,13,14};
+		
+		for(int i=0; i<=arr.length -1; i++)
+		{
+			if(arr[i] % 2==0)
+			{;
+				System.out.println(arr[i] +" "+ "is multiple of 2");
+			}
+			else
+			{
+				System.out.println(arr[i] +" "+ "is not multiple of 2");
+				
+			}
+		}
+		
+		
+
+	}
+
+
+
+
+	}
+
+
